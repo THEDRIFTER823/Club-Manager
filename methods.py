4093c8057240f2745cbbf776c240f33ef7ba4348
THEDRIFTER823 <kadenlee007@gmail.com>
2024-10-08 08:02:04 -0500

    weeeee

--- methods.py
+++ methods.py
@@ -14,9 +14,23 @@ def get_info(name):
     data = csv.DictReader(open("data.csv"))
     for line in data:
         if get_name(line["member_id"]) == name:
-            return line["member_id"], name, line["email_address"], line["phone_number"], line["home_address"]
+            return name, line["age"], line["gender"], line["email_address"], line["phone_number"], line["home_address"]
     return "Info not found for " + name
 
+def get_membership_duration(name):
+    # returns the start date and end date of someone's membership
+    data = csv.DictReader(open("data.csv"))
+    for line in data:
+        if get_name(line["member_id"]) == name:
+            return line["membership_start_date"] + " to " + line["membership_end_date"]
+
+def get_hours(name):
+    # returns the start date and end date of someone's membership
+    data = csv.DictReader(open("data.csv"))
+    for line in data:
+        if get_name(line["member_id"]) == name:
+            return line["hours_committed"] + " hours"
+        
 def get_overdue():
     # returns a list of members who's dues are not paid
     array = []
@@ -34,13 +48,6 @@ def get_paid():
         if line["payment_status"] == "Paid":
             array.append(get_name(line["member_id"]))
     return array
-
-def get_membership_duration(name):
-    # returns the start date and end date of someone's membership
-    data = csv.DictReader(open("data.csv"))
-    for line in data:
-        if get_name(line["member_id"]) == name:
-            return line["membership_start_date"] + " to " + line["membership_end_date"]
 
 def total_revenue():
     # returns the total amount of money recieved in paid fees
--- runner.py
+++ runner.py
@@ -6,6 +6,10 @@ print("1: Get information on specific members\n2: Get broader club data\n3: Clos
 
 action = int(input())
 while (action != 3):
+    if action == 0:
+        print("\nWelcome to the Club Manager Portal™!\nWe're super excited to have you!\nHere, you can:\n")
+        print("1: Get information on specific members\n2: Get broader club data\n3: Close the program\n\nType in a number to get started! (❁´◡`❁)\n")
+        action = int(input())
     if action == 1:
         print("You chose to get information on a specific member! Type in a name or member ID (1-1000) to pull data!\n")
         action = input()
@@ -17,5 +21,55 @@ while (action != 3):
                 action = input().upper()
                 if action == "Y":
                     print(get_info(name))
+                    print("\nWould you like to see their membership duration and hours committed? Y/N")
+                    action = input().upper()
+                    if action == "Y":
+                        print(get_membership_duration(name) + "\n" + get_hours(name))
+                        print("\nPress any key to go back to the main menu!\n")
+                        action = input()
+                        action = 0
+                    else:
+                        action = 0
                 else:
+                    action = 0
                     continue
+            else:
+                print("That name was not found! Type any key to go back to the main menu!\n")
+                action = input()
+                action = 0
+        else:
+            print("That's not a number! Type any key to go back to the main menu!\n")
+            action = input()
+            action = 0
+    elif action == 2:
+        print("You chose to get club data! Here, you can:\n")
+        print("1: Get members overdue on payments\n2: Get members who have paid\n3: Get the total amount of club money\n4: Get members signed up for an upcoming event\n5: Get the total hours contributed by the club\n6: Go back to the main menu!\nType in a number to get started! (¬_¬ )\n")
+        action = input()
+        while (action != 6 and action.isdigit()):
+            action = int(action)
+            if action == 1:
+                print(get_overdue() )
+                print("\nPerform another action or go back to the main menu!")
+                action = input()
+            if action == 2:
+                print(get_paid())
+                print("\nPerform another action or go back to the main menu!")
+                action = input()
+            if action == 3:
+                print(total_revenue())
+                print("\nPerform another action or go back to the main menu!")
+                action = input()
+            if action == 4:
+                print(get_signed_up())
+                print("\nPerform another action or go back to the main menu!")
+                action = input()
+            if action == 5:
+                print(total_hours())
+                print("\nPerform another action or go back to the main menu!")
+                action = input()
+        action = 0
+        continue
+    elif action == 3:
+        print("Thank you for using the Club Manager Portal™! Come again soon!")
+        quit()
+            
